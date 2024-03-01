12569cdc850594829fcf00c424d6f47787063881
axve1900 <161731827+axve1900@users.noreply.github.com>
2024-03-01 14:10:24 +0100

    Ersatte med slutlig main.js

--- js/main.js
+++ js/main.js
@@ -2,55 +2,76 @@
 "use strict";
 
 // Deklarerar baseURL
-var baseURL = "https://api.sr.se/api/v2/";
+const baseURL = "https://api.sr.se/api/v2/";
+
+// Skapar funktion som utför requests
+function requestXML(url) {
+    // Skapar en ny XMLHttpRequest
+    var request = new XMLHttpRequest();
+
+    // Requesten görs med GET från vår url, synkad request. Dvs vi väntar ut den.
+    request.open("GET", url, false);
+    request.send();
+
+    // Slutligen returnerar vi svaret
+    return request.responseXML;
+}
 
 // Vi skapar en funktion som laddar en ny beskrivning och musik
 function loadChannel(channelId) {
     // Hämtar data från SR i XML format för given kanal
-    let url = baseURL + "channels/" + channelId;
-
-    // Skapar en ny XMLHttpRequest
-    var request = new XMLHttpRequest();
-    // Requesten görs med GET från vår url, synkad request. Dvs vi väntar ut den.
-    request.open("GET", url, false);
-    request.send();
-    // Vi lagrar svaret i variabeln response
-    var response = request.responseXML;
+    var response = requestXML(baseURL + "channels/" + channelId);
     
-    // Ur response plockar vi själva kanalen ur XML datan
+    // Ur response plockar vi själva kanalen ur XML datan (deklareras som const mest för att det är 'permanent' data)
     const channelElement = response.querySelector("channel");
 
     // Skapar Header med namn
-    var channelName = document.createTextNode(channelElement.getAttribute("name"));
-    var channelHeader = document.createElement("h2"); 
+    let channelName = document.createTextNode(channelElement.getAttribute("name"));
+    let channelHeader = document.createElement("h2"); 
     channelHeader.appendChild(channelName);
 
     // Skapar Header med namn
-    var channelTagline = document.createTextNode(channelElement.querySelector("tagline").textContent);
-    var channelContent = document.createElement("h3"); 
+    let channelTagline = document.createTextNode(channelElement.querySelector("tagline").textContent);
+    let channelContent = document.createElement("h3"); 
     channelContent.appendChild(channelTagline);
-        
-    // Ersätter nuvarande kanalinfo
-    document.getElementById("info").replaceChildren(channelHeader, channelTagline);
 
-    // Ersätter nuvarande ljudkälla
+    // Ersätter nuvarande kanalinfo
+    document.getElementById("info").replaceChildren(channelHeader, channelContent, document.createElement("hr"));
+
+    // Hämtar det som spelas nu
+    var response = requestXML(baseURL + "playlists/rightnow?channelid=" + channelId);
+
+    // Ur response plockar vi det som spelas nu
+    const playlist = response.querySelector("playlist");
+
+    // Vi skapar föregående och ny song
+    var previoussong = playlist.querySelector("previoussong");
+    var lastsong = document.createTextNode("Föregående: " + previoussong.querySelector("description").textContent);
+    var currentsong = playlist.querySelector("song");
+    var newsong = document.createTextNode("Kommande: " + currentsong.querySelector("description").textContent);
+
+    // Lägger till dessa och ett nytt br element till p
+    var p = document.createElement("p"); 
+    p.appendChild(lastsong);
+    p.appendChild(document.createElement("br"));
+    p.appendChild(newsong);
+
+    // Sen lägger vi till detta till inforutan
+    document.getElementById("info").appendChild(p);
+
+
+    // Slutligen ersätter vi nuvarande ljudkälla
     audioElement.src = 'http://sverigesradio.se/topsy/direkt/' + channelId + '-hi.mp3';
     audioElement.play();
 }
 
 // Vi skapar en funktion som laddar en ny tablå men inte laddar in nytt ljud
 function loadTabla(channelId) {
-    // Hämtar alla program i dagens tablå (enligt dokumentation borde '&size=200' inte behövas, men utan denna hämtas bara en del av dagens tablå)
+    // deklarerar day (dagens datum) som en const
     const day = new Date().toISOString().split('T')[0];
-    let url = baseURL + "scheduledepisodes?channelid=" + channelId + "&date=" + day + "&size=200";
 
-    // Skapar en ny XMLHttpRequest
-    var request = new XMLHttpRequest();
-    // Requesten görs med GET från vår url, synkad request. Dvs vi väntar ut den.
-    request.open("GET", url, false);
-    request.send();
-    // Vi lagrar svaret i variabeln response
-    var response = request.responseXML;
+    // Hämtar alla program i dagens tablå (enligt dokumentation borde '&size=200' inte behövas, men utan denna hämtas bara en del av dagens tablå)
+    var response = requestXML(baseURL + "scheduledepisodes?channelid=" + channelId + "&date=" + day + "&size=200");
     
     // Därefter gör vi om detta till en list av alla element (egentligen en NodeList, ett slags list-element)
     const scheduleList = response.querySelectorAll('scheduledepisode');
@@ -60,20 +81,21 @@ function loadTabla(channelId) {
     
     for(var i=0; i < scheduleList.length; i++){
         // Hämtar titel och beskrivning
-        var scheduleTitle = document.createTextNode(scheduleList[i].querySelector("title").textContent);
-        var scheduleHeader = document.createElement("h2");
+        let scheduleTitle = document.createTextNode(scheduleList[i].querySelector("title").textContent);
+        let scheduleHeader = document.createElement("h2");
         scheduleHeader.appendChild(scheduleTitle);
-        var scheduleDescription = document.createTextNode(scheduleList[i].querySelector("description").textContent);
-        var scheduleContent = document.createElement("h3"); 
+        let scheduleDescription = document.createTextNode(scheduleList[i].querySelector("description").textContent);
+        let scheduleContent = document.createElement("h3"); 
         scheduleContent.appendChild(scheduleDescription);
 
-        // Hämtar datum
-        var scheduleStart = document.createTextNode(scheduleList[i].querySelector("starttimeutc").textContent);
-        var scheduleTime = document.createElement("p");
+        // Tar ut starttid
+        let scheduleStart = document.createTextNode(scheduleList[i].querySelector("starttimeutc").textContent);
         // Gör om till läsbart datum
-        var parsedDate = new Date(scheduleStart.data);
-        var formattedDate = parsedDate.toLocaleString();
-        var dateTextNode = document.createTextNode(formattedDate);
+        let formattedDate = new Date(scheduleStart.data).toLocaleString();
+        // Gör om detta till en 'node'
+        let dateTextNode = document.createTextNode(formattedDate);
+        // Lägger till node till p element
+        let scheduleTime = document.createElement("p");
         scheduleTime.appendChild(dateTextNode);
 
         // Lägger till data och linje
